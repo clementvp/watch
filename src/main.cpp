7244cedc1b365b164c38882e79efff2db19bef80
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-07-21 11:21:30 +0200

    get a fonctionnal clock

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
+#include <M5Unified.h>
+int currentPage = 0;  // 0 pour l'affichage de l'heure, 1 pour le réglage
+int settingIndex = 0; // 0 pour les heures, 1 pour les minutes
+int hours = 0, minutes = 0, seconds = 0;
+bool needUpdate = true;
+char lastTimeStr[9] = "";
 
-// put function declarations here:
-int myFunction(int, int);
+void displayTime()
+{
+  auto dt = M5.Rtc.getDateTime();
+  char timeStr[9];
+  snprintf(timeStr, sizeof(timeStr), "%02d:%02d:%02d", dt.time.hours, dt.time.minutes, dt.time.seconds);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  if (strcmp(timeStr, lastTimeStr) != 0)
+  {
+    M5.Lcd.setTextColor(WHITE, BLACK); // Texte blanc sur fond noir
+    M5.Lcd.setTextSize(4);
+    M5.Lcd.setTextDatum(MC_DATUM);
+    M5.Lcd.drawString(timeStr, M5.Lcd.width() / 2, M5.Lcd.height() / 2);
+    strcpy(lastTimeStr, timeStr);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setTime()
+{
+  M5.Display.setCursor(0, 0);
+  M5.Display.setTextSize(2);
+  M5.Display.printf("Set time");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  M5.Rtc.begin();
+  M5.Lcd.setRotation(1);
+}
+
+void loop()
+{
+
+  M5.update();
+
+  if (M5.BtnA.wasPressed())
+  {
+    M5.Lcd.clear();
+    currentPage = 1 - currentPage; // Basculer entre les pages
+    settingIndex = 0;              // Réinitialiser l'index de réglage
+  }
+
+  if (currentPage == 0)
+  {
+    displayTime();
+  }
+  else
+  {
+    setTime();
+  }
+  delay(100);
 }
