1fadf927709d60d7544375f4de2488f4e03e6e71
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-07-22 21:06:44 +0200

    big refacto

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,15 @@
 #include "pages/DisplayTime/DisplayTime.h"
 #include "pages/SetTime/SetTime.h"
 
-int currentPage = 0;
+const unsigned long SLEEP_TIMEOUT = 60000; // 1 minute en millisecondes
+const int DISPLAY_PAGE = 0;
+const int SET_TIME_PAGE = 1;
+const int SET_DATE_PAGE = 2;
+
+int currentPage = DISPLAY_PAGE;
+unsigned long displayPageEntryTime = 0;
+unsigned long lastInteractionTime = 0;
+
 void setup()
 {
   auto cfg = M5.config();
@@ -11,40 +19,77 @@ void setup()
   M5.Rtc.begin();
   M5.Lcd.setRotation(1);
   M5.Lcd.setBrightness(1);
+  lastInteractionTime = millis();
 }
 
 void goToSleep()
 {
-
   M5.Power.powerOff();
+}
+
+bool isButtonPressed()
+{
+  return M5.BtnA.wasPressed() || M5.BtnB.wasPressed() || M5.BtnPWR.wasPressed();
+}
+
+bool isDisplayPageActive()
+{
+  return currentPage == DISPLAY_PAGE;
+}
+
+bool isTimeToSleep()
+{
+  unsigned long currentTime = millis();
+  return isDisplayPageActive() &&
+         currentTime - displayPageEntryTime > SLEEP_TIMEOUT &&
+         currentTime - lastInteractionTime > SLEEP_TIMEOUT;
+}
+
+void handlePageChange(int &previousPage)
+{
+  if (isDisplayPageActive() && previousPage != DISPLAY_PAGE)
+  {
+    displayPageEntryTime = lastInteractionTime;
+  }
+  previousPage = currentPage;
+}
+
+void updateLastInteractionTime()
+{
+  if (isButtonPressed())
+  {
+    lastInteractionTime = millis();
+  }
+}
+
+void handleCurrentPage()
+{
+  switch (currentPage)
+  {
+  case DISPLAY_PAGE:
+    displayTime();
+    break;
+  case SET_TIME_PAGE:
+    setTime();
+    break;
+  case SET_DATE_PAGE:
+    setDate();
+    break;
+  }
 }
 
 void loop()
 {
-  static unsigned long lastActivityTime = millis();
+  static int previousPage = -1;
 
-  if (M5.BtnA.wasPressed() || M5.BtnB.wasPressed() || M5.BtnPWR.wasPressed())
-  {
-    lastActivityTime = millis();
-  }
+  M5.update();
+  updateLastInteractionTime();
+  handlePageChange(previousPage);
 
-  if (millis() - lastActivityTime > 60000)
+  if (isTimeToSleep())
   {
     goToSleep();
   }
 
-  M5.update();
-
-  if (currentPage == 0)
-  {
-    displayTime();
-  }
-  else if (currentPage == 1)
-  {
-    setTime();
-  }
-  else if (currentPage == 2)
-  {
-    setDate();
-  }
+  handleCurrentPage();
 }
