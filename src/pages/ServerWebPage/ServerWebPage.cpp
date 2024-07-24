29b0d91a3c23fd65f221985d5f295b653b7470e3
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-07-24 21:29:14 +0200

    implement wifi listing

--- src/pages/ServerWebPage/ServerWebPage.cpp
+++ src/pages/ServerWebPage/ServerWebPage.cpp
@@ -18,9 +18,36 @@ void setupDisplay()
     M5.Lcd.drawString("Wifi AP", M5.Lcd.width() / 2, M5.Lcd.height() / 2);
 }
 
+String getWifiListOptions()
+{
+    String wifiList = "";
+    int n = WiFi.scanNetworks();
+    for (int i = 0; i < n; ++i)
+    {
+        wifiList += "<option value=\"" + WiFi.SSID(i) + "\">" + WiFi.SSID(i) + "</option>";
+        delay(10);
+    }
+    return wifiList;
+}
+
 void handleRoot()
 {
-    server.send(200, "text/html", "<html><body><h1>Hello</h1></body></html>");
+    String html = "<!DOCTYPE html>"
+                  "<html lang='fr'>"
+                  "<head>"
+                  "<meta charset='UTF-8'>"
+                  "<title>Réseaux Wi-Fi disponibles</title>"
+                  "</head>"
+                  "<body>"
+                  "<h1>Réseaux Wi-Fi disponibles</h1>"
+                  "<form>"
+                  "<select name='wifi'>" +
+                  getWifiListOptions() +
+                  "</select>"
+                  "<input type='submit' value='Connecter'>"
+                  "</form>"
+                  "</body></html>";
+    server.send(200, "text/html; charset=utf-8", html);
 }
 
 void configureServer()
