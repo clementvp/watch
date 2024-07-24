c4838f9dcdd16754b9bb21103ac2f2b996804a89
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-07-24 21:47:22 +0200

    refacto sleep logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,7 @@ const int DISPLAY_PAGE = 0;
 const int SET_TIME_PAGE = 1;
 const int SET_DATE_PAGE = 2;
 const int SERVER_WEB_PAGE = 3;
-
 int currentPage = DISPLAY_PAGE;
-unsigned long displayPageEntryTime = 0;
 unsigned long lastInteractionTime = 0;
 
 void setup()
@@ -28,36 +26,14 @@ void goToSleep()
   M5.Power.powerOff();
 }
 
-bool isButtonPressed()
-{
-  return M5.BtnA.wasPressed() || M5.BtnB.wasPressed() || M5.BtnPWR.wasPressed();
-}
-
-bool isDisplayPageActive()
-{
-  return currentPage == DISPLAY_PAGE;
-}
-
 bool isTimeToSleep()
 {
-  unsigned long currentTime = millis();
-  return isDisplayPageActive() &&
-         currentTime - displayPageEntryTime > SLEEP_TIMEOUT &&
-         currentTime - lastInteractionTime > SLEEP_TIMEOUT;
-}
-
-void handlePageChange(int &previousPage)
-{
-  if (isDisplayPageActive() && previousPage != DISPLAY_PAGE)
-  {
-    displayPageEntryTime = lastInteractionTime;
-  }
-  previousPage = currentPage;
+  millis() - lastInteractionTime > SLEEP_TIMEOUT;
 }
 
 void updateLastInteractionTime()
 {
-  if (isButtonPressed())
+  if (currentPage != DISPLAY_PAGE)
   {
     lastInteractionTime = millis();
   }
@@ -84,11 +60,8 @@ void handleCurrentPage()
 
 void loop()
 {
-  static int previousPage = -1;
-
   M5.update();
   updateLastInteractionTime();
-  handlePageChange(previousPage);
 
   if (isTimeToSleep())
   {
