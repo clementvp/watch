eeb3bcbb1aee0302b2551c52e6c150205328af81
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-07-26 14:59:12 +0200

    change html

--- src/pages/ServerWebPage/ServerWebPage.cpp
+++ src/pages/ServerWebPage/ServerWebPage.cpp
@@ -40,6 +40,7 @@ void handleRoot()
                   "<html lang='fr'>"
                   "<head>"
                   "<meta charset='UTF-8'>"
+                  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
                   "<title>Réseaux Wi-Fi disponibles</title>"
                   "</head>"
                   "<body>"
